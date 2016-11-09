8e610cebc82181b04a9234781f4eb1e4f8c04fc7
Daniel Fairhead <daniel.fairhead@om.org>
2016-11-09 10:52:04 +0000

    pep8 + pylint happiness. (formatting and cleaning)

--- simpleeval.py
+++ simpleeval.py
@@ -88,7 +88,7 @@ from random import random
 # Module wide 'globals'
 
 MAX_STRING_LENGTH = 100000
-MAX_POWER = 4000000 # highest exponent
+MAX_POWER = 4000000  # highest exponent
 DISALLOW_PREFIXES = ['_', 'func_']
 
 PYTHON3 = sys.version_info[0] == 3
@@ -96,68 +96,80 @@ PYTHON3 = sys.version_info[0] == 3
 ########################################
 # Exceptions:
 
+
 class InvalidExpression(Exception):
     ''' Generic Exception '''
     pass
+
 
 class FunctionNotDefined(InvalidExpression):
     ''' sorry! That function isn't defined! '''
     def __init__(self, func_name, expression):
         self.message = "Function '{0}' not defined," \
-                       " for expression '{1}'.".format( func_name, expression)
+                       " for expression '{1}'.".format(func_name, expression)
         self.func_name = func_name
         self.expression = expression
 
         # pylint: disable=bad-super-call
         super(InvalidExpression, self).__init__(self.message)
 
+
 class NameNotDefined(InvalidExpression):
     ''' a name isn't defined. '''
     def __init__(self, name, expression):
         self.message = "'{0}' is not defined for expression '{1}'".format(
-                            name, expression)
+            name, expression)
         self.name = name
         self.expression = expression
 
         # pylint: disable=bad-super-call
         super(InvalidExpression, self).__init__(self.message)
 
+
 class AttributeDoesNotExist(InvalidExpression):
     '''attribute does not exist'''
     def __init__(self, attr, expression):
-        self.message = "Attribute '{0}' does not exist in expression '{1}'".format(
-            attr, expression)
+        self.message = \
+            "Attribute '{0}' does not exist in expression '{1}'".format(
+                attr, expression)
         self.attr = attr
         self.expression = expression
+
 
 class FeatureNotAvailable(InvalidExpression):
     ''' What you're trying to do is not allowed. '''
     pass
+
 
 class NumberTooHigh(InvalidExpression):
     ''' Sorry! That number is too high. I don't want to spend the
         next 10 years evaluating this expression! '''
     pass
 
+
 class StringTooLong(InvalidExpression):
     ''' That string is **way** too long, baby. '''
     pass
 
+
 ########################################
 # Default simple functions to include:
+
 
 def random_int(top):
     ''' return a random int below <top> '''
     return int(random() * top)
 
-def safe_power(a, b): # pylint: disable=invalid-name
+
+def safe_power(a, b):  # pylint: disable=invalid-name
     ''' a limited exponent/to-the-power-of function, for safety reasons '''
     if abs(a) > MAX_POWER or abs(b) > MAX_POWER:
         raise NumberTooHigh("Sorry! I don't want to evaluate {0} ** {1}"
                             .format(a, b))
     return a ** b
 
-def safe_mult(a, b): # pylint: disable=invalid-name
+
+def safe_mult(a, b):  # pylint: disable=invalid-name
     ''' limit the number of times a string can be repeated... '''
     if isinstance(a, str) or isinstance(b, str):
         if isinstance(a, int) and a*len(b) > MAX_STRING_LENGTH:
@@ -167,7 +179,8 @@ def safe_mult(a, b): # pylint: disable=invalid-name
 
     return a * b
 
-def safe_add(a, b): # pylint: disable=invalid-name
+
+def safe_add(a, b):  # pylint: disable=invalid-name
     ''' string length limit again '''
     if isinstance(a, str) and isinstance(b, str):
         if len(a) + len(b) > MAX_STRING_LENGTH:
@@ -181,19 +194,22 @@ def safe_add(a, b): # pylint: disable=invalid-name
 
 DEFAULT_OPERATORS = {ast.Add: safe_add, ast.Sub: op.sub, ast.Mult: safe_mult,
                      ast.Div: op.truediv, ast.Pow: safe_power, ast.Mod: op.mod,
-                     ast.Eq: op.eq, ast.NotEq: op.ne, ast.Gt: op.gt, ast.Lt: op.lt,
+                     ast.Eq: op.eq, ast.NotEq: op.ne,
+                     ast.Gt: op.gt, ast.Lt: op.lt,
                      ast.GtE: op.ge, ast.LtE: op.le, ast.USub: op.neg,
                      ast.UAdd: op.pos}
 
 DEFAULT_FUNCTIONS = {"rand": random, "randint": random_int,
-                     "int": int, "float": float, "str": str if PYTHON3 else unicode}
+                     "int": int, "float": float,
+                     "str": str if PYTHON3 else unicode}
 
 DEFAULT_NAMES = {"True": True, "False": False}
 
 ########################################
 # And the actual evaluator:
 
-class SimpleEval(object): # pylint: disable=too-few-public-methods
+
+class SimpleEval(object):  # pylint: disable=too-few-public-methods
     ''' A very simple expression parser.
         >>> s = SimpleEval()
         >>> s.eval("20 + 30 - ( 10 * 5)")
@@ -230,77 +246,79 @@ class SimpleEval(object): # pylint: disable=too-few-public-methods
 
     # pylint: disable=too-many-return-statements, too-many-branches
     def _eval(self, node):
-        ''' The internal eval function used on each node in the parsed tree. '''
+        ''' The internal evaluator used on each node in the parsed tree. '''
 
         # literals:
 
-        if isinstance(node, ast.Num): # <number>
+        if isinstance(node, ast.Num):  # <number>
             return node.n
-        elif isinstance(node, ast.Str): # <string>
+        elif isinstance(node, ast.Str):  # <string>
             if len(node.s) > MAX_STRING_LENGTH:
                 raise StringTooLong("String Literal in statement is too long!"
                                     " ({0}, when {1} is max)".format(
-                                    len(node.s), MAX_STRING_LENGTH))
+                                        len(node.s), MAX_STRING_LENGTH))
             return node.s
 
         # python 3 compatibility:
 
         elif (hasattr(ast, 'NameConstant') and
-                isinstance(node, ast.NameConstant)): # <bool>
+              isinstance(node, ast.NameConstant)):  # <bool>
             return node.value
 
         # operators, functions, etc:
 
-        elif isinstance(node, ast.UnaryOp): # - and + etc.
+        elif isinstance(node, ast.UnaryOp):  # - and + etc.
             return self.operators[type(node.op)](self._eval(node.operand))
-        elif isinstance(node, ast.BinOp): # <left> <operator> <right>
+        elif isinstance(node, ast.BinOp):  # <left> <operator> <right>
             return self.operators[type(node.op)](self._eval(node.left),
-                                       self._eval(node.right))
-        elif isinstance(node, ast.BoolOp): # and & or...
+                                                 self._eval(node.right))
+        elif isinstance(node, ast.BoolOp):  # and & or...
             if isinstance(node.op, ast.And):
-                for v in node.values:
-                    vout = self._eval(v)
+                for value in node.values:
+                    vout = self._eval(value)
                     if not vout:
                         return False
                 return vout
             elif isinstance(node.op, ast.Or):
-                for v in node.values:
-                    n = self._eval(v)
-                    if n:
-                        return n
+                for value in node.values:
+                    vout = self._eval(value)
+                    if vout:
+                        return vout 
                 return False
-        elif isinstance(node, ast.Compare): # 1 < 2, a == b...
-            x = self._eval(node.left)
-            for op, comp  in zip(node.ops, node.comparators):
-                y = self._eval(comp)
-                if self.operators[type(op)](x, y):
-                    x = y
+
+        elif isinstance(node, ast.Compare):  # 1 < 2, a == b...
+            left = self._eval(node.left)
+            for operation, comp in zip(node.ops, node.comparators):
+                right = self._eval(comp)
+                if self.operators[type(operation)](left, right):
+                    left = right  # Hi Dr. Seuss...
                 else:
                     return False
             return True
 
-        elif isinstance(node, ast.IfExp): # x if y else z
+        elif isinstance(node, ast.IfExp):  # x if y else z
             return self._eval(node.body) if self._eval(node.test) \
                                          else self._eval(node.orelse)
-        elif isinstance(node, ast.Call): # function...
+        elif isinstance(node, ast.Call):  # function...
             try:
-                if type(node.func) == ast.Name:
+                if isinstance(node.func, ast.Name):
                     return self.functions[node.func.id](*(self._eval(a)
                                                           for a in node.args))
-                elif type(node.func) == ast.Attribute:
+                elif isinstance(node.func, ast.Attribute):
                     return self._eval(node.func)(*(self._eval(a)
-                                                        for a in node.args))
+                                                   for a in node.args))
             except KeyError:
                 raise FunctionNotDefined(node.func.id, self.expr)
 
         # variables/names:
 
-        elif isinstance(node, ast.Name): # a, b, c...
+        elif isinstance(node, ast.Name):  # a, b, c...
             try:
-                #This happens at least for slicing
-                #This is a safe thing to do because it is impossible
-                #that there is a true exression assigning to none
-                #(the compiler rejects it, so you can't even pass that to ast.parse)
+                # This happens at least for slicing
+                # This is a safe thing to do because it is impossible
+                # that there is a true exression assigning to none
+                # (the compiler rejects it, so you can't even pass that
+                # to ast.parse)
                 if node.id == "None":
                     return None
                 elif isinstance(self.names, dict):
@@ -308,21 +326,24 @@ class SimpleEval(object): # pylint: disable=too-few-public-methods
                 elif callable(self.names):
                     return self.names(node)
                 else:
-                    raise InvalidExpression('Trying to use name (variable) "{0}"'
-                                            ' when no "names" defined for'
-                                            ' evaluator'.format(node.id))
+                    raise InvalidExpression(
+                        'Trying to use name (variable) "{0}"'
+                        ' when no "names" defined for'
+                        ' evaluator'.format(node.id))
 
             except KeyError:
                 raise NameNotDefined(node.id, self.expr)
 
-        elif isinstance(node, ast.Subscript): # b[1]
+        elif isinstance(node, ast.Subscript):  # b[1]
             return self._eval(node.value)[self._eval(node.slice)]
 
-        elif isinstance(node, ast.Attribute): # a.b.c
+        elif isinstance(node, ast.Attribute):  # a.b.c
             for prefix in DISALLOW_PREFIXES:
                 if node.attr.startswith(prefix):
-                 raise FeatureNotAvailable("Sorry, access to __attributes or "
-                    "func_ attributes is not available. ({0})".format(node.attr))
+                    raise FeatureNotAvailable(
+                        "Sorry, access to __attributes "
+                        " or func_ attributes is not available. "
+                        "({0})".format(node.attr))
 
             try:
                 return self._eval(node.value)[node.attr]
@@ -351,7 +372,8 @@ class SimpleEval(object): # pylint: disable=too-few-public-methods
             return slice(lower, upper, step)
         else:
             raise FeatureNotAvailable("Sorry, {0} is not available in this "
-                                      "evaluator".format(type(node).__name__ ))
+                                      "evaluator".format(type(node).__name__))
+
 
 def simple_eval(expr, operators=None, functions=None, names=None):
     ''' Simply evaluate an expresssion '''
