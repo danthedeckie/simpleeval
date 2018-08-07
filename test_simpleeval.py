2769106aa4fcb152dc75f3cc94719a60310fcbdd
Daniel Fairhead <danthedeckie@gmail.com>
2018-08-06 19:46:42 -0500

    Add method chaining test - more would be good.

--- test_simpleeval.py
+++ test_simpleeval.py
@@ -665,6 +665,29 @@ class TestSimpleEval(unittest.TestCase):
         self.assertEqual(simple_eval('randint(200) < 200 and rand() > 0'), True)
 
 
+class TestMethodChaining(unittest.TestCase):
+    def test_chaining_correct(self):
+        """
+            Contributed by Khalid Grandi (xaled).
+        """
+        class A(object):
+            def __init__(self):
+                self.a = "0"
+
+            def add(self, b):
+                self.a += "-add" + str(b)
+                return self
+
+            def sub(self, b):
+                self.a += "-sub" + str(b)
+                return self
+
+            def tostring(self):
+                return str(self.a)
+
+        x = A()
+        self.assertEqual(simple_eval("x.add(1).sub(2).sub(3).tostring()", names={"x": x}), "0-add1-sub2-sub3")
+
 class TestExtendingClass(unittest.TestCase):
     """
         It should be pretty easy to extend / inherit from the SimpleEval class,
