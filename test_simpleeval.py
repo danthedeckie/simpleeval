a456c6b4c981b5d94e38c9d762aec05b48a82e17
Andrew Zhu <me@andrew-zhu.com>
2019-10-22 13:41:12 -0700

    add tests

--- test_simpleeval.py
+++ test_simpleeval.py
@@ -582,25 +582,50 @@ class TestComprehensions(DRYTest):
 
     def test_basic(self):
         self.t('[a + 1 for a in [1,2,3]]', [2,3,4])
+        self.t('{a + 1 for a in [1,2,3]}', {2, 3, 4})
+        self.t('{a + 1: a - 1 for a in [1,2,3]}', {2: 0, 3: 1, 4: 2})
+
+    def test_setcomp(self):
+        self.t('{0 for a in [1,2,3]}', {0})
+        self.t('{a % 10 for a in [5,10,15,20]}', {0, 5})
+
+    def test_genexp(self):
+        self.t('list(a + 1 for a in [1,2,3])', [2, 3, 4])
 
     def test_with_self_reference(self):
         self.t('[a + a for a in [1,2,3]]', [2,4,6])
+        self.t('{a + a for a in [1,2,3]}', {2, 4, 6})
+        self.t('{a: a for a in [1,2,3]}', {1:1, 2:2, 3:3})
 
     def test_with_if(self):
         self.t('[a for a in [1,2,3,4,5] if a <= 3]', [1,2,3])
+        self.t('{a for a in [1,2,3,4,5] if a <= 3}', {1, 2, 3})
+        self.t('{a: a for a in [1,2,3,4,5] if a <= 3}', {1:1, 2:2, 3:3})
 
     def test_with_multiple_if(self):
-        self.t('[a for a in [1,2,3,4,5] if a <= 3 and a > 1 ]', [2,3])
+        self.t('[a for a in [1,2,3,4,5] if a <= 3 if a > 1 ]', [2,3])
+        self.t('{a for a in [1,2,3,4,5] if a <= 3 if a > 1 }', {2, 3})
+        self.t('{a:a for a in [1,2,3,4,5] if a <= 3 if a > 1 }', {2:2, 3:3})
 
     def test_attr_access_fails(self):
         with self.assertRaises(FeatureNotAvailable):
             self.t('[a.__class__ for a in [1,2,3]]', None)
+        with self.assertRaises(FeatureNotAvailable):
+            self.t('{a.__class__ for a in [1,2,3]}', None)
+        with self.assertRaises(FeatureNotAvailable):
+            self.t('{a.__class__: a for a in [1,2,3]}', None)
+        with self.assertRaises(FeatureNotAvailable):
+            self.t('{a: a.__class__ for a in [1,2,3]}', None)
 
     def test_unpack(self):
         self.t('[a+b for a,b in ((1,2),(3,4))]', [3, 7])
+        self.t('{a+b for a,b in ((1,2),(3,4))}', {3, 7})
+        self.t('{a: b for a,b in ((1,2),(3,4))}', {1:2, 3:4})
 
     def test_nested_unpack(self):
         self.t('[a+b+c for a, (b, c) in ((1,(1,1)),(3,(2,2)))]', [3, 7])
+        self.t('{a+b+c for a, (b, c) in ((1,(1,1)),(3,(2,2)))}', {3, 7})
+        self.t('{a:b+c for a, (b, c) in ((1,(1,1)),(3,(2,2)))}', {1:2, 3:4})
 
     def test_other_places(self):
         self.s.functions = {'sum': sum}
@@ -618,10 +643,18 @@ class TestComprehensions(DRYTest):
         self.s.functions = {'range': range}
         s = '[j for i in range(100) if i > 10 for j in range(i) if j < 20]'
         self.t(s, eval(s))
+        s = '{j for i in range(100) if i > 10 for j in range(i) if j < 20}'
+        self.t(s, eval(s))
+        s = '{j:i for i in range(100) if i > 10 for j in range(i) if j < 20}'
+        self.t(s, eval(s))
 
     def test_triple_generators(self):
         self.s.functions = {'range': range}
         s = '[(a,b,c) for a in range(4) for b in range(a) for c in range(b)]'
+        self.t(s, eval(s))
+        s = '{(a,b,c) for a in range(4) for b in range(a) for c in range(b)}'
+        self.t(s, eval(s))
+        s = '{a+b+c:(a,b,c) for a in range(4) for b in range(a) for c in range(b)}'
         self.t(s, eval(s))
 
     def test_too_long_generator(self):
