cc7b7dfb755a416cb343b32b50ab776160d8f9fb
Andrew Zhu <me@andrew-zhu.com>
2019-10-25 12:48:05 -0700

    add executors capable of control

--- executors.py
+++ executors.py
@@ -1,31 +1,93 @@
 import ast
 
-from simpleeval import CompoundEvalWithAssignments, FeatureNotAvailable
+from simpleeval import CompoundEvalWithAssignments, EvalWithAssignments
 
 
-class SimpleExecutor(CompoundEvalWithAssignments):
+class SimpleExecutor(EvalWithAssignments):
     def __init__(self, operators=None, functions=None, names=None):
         super(SimpleExecutor, self).__init__(operators, functions, names)
-
-    def eval(self, expr):  # allow for ast.Assign to set names
-        self.expr = expr
-        expression = ast.parse(expr.strip()).body[0]
-        return self._eval_expression(expression)
 
     def exec(self, expr):
         self.expr = expr
         body = ast.parse(expr.strip()).body
-        for expression in body:
-            self._eval_expression(expression)
+        return self._exec(body)
 
-    def _eval_expression(self, expression):
-        if isinstance(expression, ast.Expr):
-            return self._eval(expression.value)
-        elif isinstance(expression, ast.Assign):
-            for target in expression.targets:  # a = b = 1
-                self._assign(target, expression.value)
-        elif isinstance(expression, ast.AugAssign):  # a += 1
-            self._aug_assign(expression.target, expression.op, expression.value)
-        # TODO py 3.8 walrus op
+    def _exec(self, body):
+        for expression in body:
+            try:
+                self._eval(expression)
+            except Return as r:
+                return r.value
+
+
+class ExecutorWithControl(SimpleExecutor, CompoundEvalWithAssignments):
+    def __init__(self, operators=None, functions=None, names=None):
+        super(ExecutorWithControl, self).__init__(operators, functions, names)
+
+        self.nodes.update({
+            ast.If: self._exec_if,
+            ast.For: self._exec_for,
+            ast.While: self._exec_while,
+            Value: lambda val: val.value,
+            ast.Break: self._exec_break,
+            ast.Continue: self._exec_continue,
+            ast.Return: self._exec_return
+        })
+
+    def _exec_if(self, node):
+        test = self._eval(node.test)
+        if test:
+            self._exec(node.body)
         else:
-            raise FeatureNotAvailable("Unknown ast body type: {}".format(type(expression).__name__))
+            self._exec(node.orelse)
+
+    def _exec_for(self, node):
+        for item in self._eval(node.iter):
+            self._assign(node.target, Value(item))
+            try:
+                self._exec(node.body)
+            except Break:
+                break
+            except Continue:
+                continue
+        else:
+            self._exec(node.orelse)
+
+    def _exec_while(self, node):
+        while self._eval(node.test):
+            try:
+                self._exec(node.body)
+            except Break:
+                break
+            except Continue:
+                continue
+        else:
+            self._exec(node.orelse)
+
+    def _exec_break(self, node):
+        raise Break
+
+    def _exec_continue(self, node):
+        raise Continue
+
+    def _exec_return(self, node):
+        raise Return(self._eval(node.value))
+
+
+class Value:  # used to pass already-evaluated values to assignments
+    def __init__(self, value):
+        self.value = value
+
+
+# exceptions used to propogate loop-breaking signals
+class Return(BaseException):
+    def __init__(self, retval):
+        self.value = retval
+
+
+class Break(BaseException):
+    pass
+
+
+class Continue(BaseException):
+    pass
