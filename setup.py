2d17d36f68a3925c4c3a7aa5d4a83ca4a1462791
KOLANICH <kolan_n@mail.ru>
2021-01-26 12:24:51 +0300

    Moved the metadata into setup.cfg Added pyproject.toml The version is now fetched from git tags. Made the wheel universal.

--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
+#!/usr/bin/env python3
+from os.path import dirname
+from os.path import join as pjoin
+
 from setuptools import setup
+from setuptools.config import read_configuration
 
-__version__ = '0.9.10'
+import setuptools_scm
 
-setup(
-    name='simpleeval',
-    py_modules=['simpleeval'],
-    version=__version__,
-    description='A simple, safe single expression evaluator library.',
-    long_description=open('README.rst', 'r').read(),
-    long_description_content_type='text/x-rst',
-    author='Daniel Fairhead',
-    author_email='danthedeckie@gmail.com',
-    url='https://github.com/danthedeckie/simpleeval',
-    download_url='https://github.com/danthedeckie/simpleeval/tarball/' + __version__,
-    keywords=['eval', 'simple', 'expression', 'parse', 'ast'],
-    test_suite='test_simpleeval',
-    classifiers=['Development Status :: 4 - Beta',
-                 'Intended Audience :: Developers',
-                 'License :: OSI Approved :: MIT License',
-                 'Topic :: Software Development :: Libraries :: Python Modules',
-                 'Programming Language :: Python',
-                 ],
-)
+if __name__ == "__main__":
+    p = dirname(__file__)
+    setup_dict = read_configuration(pjoin(p, "setup.cfg"))
+    v = setuptools_scm.version_from_scm(str(p))
+    __version__ = setuptools_scm.format_version(
+        v,
+        version_scheme=setuptools_scm.DEFAULT_VERSION_SCHEME,
+        local_scheme=setuptools_scm.DEFAULT_LOCAL_SCHEME,
+    )
+    setup_dict = {
+        "version": __version__,
+    }
+    if v.exact:
+        setup_dict["download_url"] = setup_dict["url"] + "/tarball/" + str(v.tag)
+    setup(**setup_dict)
